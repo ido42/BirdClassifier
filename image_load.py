7df3da593881d890f01797630550d324a9f7d7af
idil <ipkatac@gmail.com>
2021-03-30 11:43:18 +0300

    Initial commit

---
+++ image_load.py
@@ -0,0 +1,8 @@
+import os
+ProjectFolder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+directory_name=os.path.dirname
+imgFolder = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images"))
+imgTrain = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images", "Train"))
+
+imgTrainFlamingo = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images", "Train", "FLAMINGO"))
+imgTrainBarnOwl = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images", "Train", "BARN OWL"))
