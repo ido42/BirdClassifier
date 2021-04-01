9a9c802686830fccc179f8b3304c1f6af6526c77
idil <ipkatac@gmail.com>
2021-04-01 19:28:14 +0300

    all in matrix form

--- Classes/ANN.py
+++ Classes/ANN.py
@@ -1,33 +1,40 @@
 from Classes.Layer import*
-from Classes.Neuron import*
+
 
 class ANN():
-    def __init__(self, num_input, num_output, num_layer):
-        self.num_input = num_input
-        self.num_output = num_output
+    def __init__(self,num_layer,learning_rate,layers_neurons): #layers_neurons is a list
         self.num_layer = num_layer
+        self.l_rate = learning_rate
         self.layers = []
-        for layer in range(num_layer):
-            self.neurons.append(Layer(self.node_count))
-
-    def add_layer(self, num_new_layer):
-        for layer in range(num_new_layer):
-            self.layers = self.layers.append(Layer(self.inputs, self.weights))
-        self.num_layer += num_new_layer
-
-    def remove_layer(self,num_remove_layer):
-        for layer in range(num_remove_layer):
-            self.layers = self.layers.append(Layer(self.inputs, self.weights))
-        self.num_layer -= num_remove_layer
+        for l in range(num_layer):
+            self.layers.append(Layer(layers_neurons[l],layers_neurons[l+1]))
 
     def forward_pass(self, flat_image):  # takes flat image calculates the output once, using the current weights
-        self.layers[1].input_vector = flat_image
+        self.layers[1].take_input(flat_image)
+
         for i in range(1, len(self.layers)):
-            self.layer[i].input_vector = self.layer[i-1].output_vector
+            sigm_out = 1/(np.exp(-self.layers[i-1].layer_output()))
+            self.layers[i].take_input(sigm_out)
+        self.layers[-1].take_input(1/(np.exp(-self.layers[-2].layer_output())))
 
-    def back_prop(self, result):
-
+    def back_prop(self, result): # result is in the form of a vector which is the same size with output
         pass
-    def train(self, flat_image):  # takes one flatten image, updates weights using forward_pass and back_prop
+        #dLoss =
+        #self.layers
+        #for l in range(0, len(self.layers), -1): # backwards
+         #   dLoss=
 
+
+
+    def train(self, flat_image, result):  # takes one flatten image, updates weights using forward_pass and back_prop
         pass
+
+#    def add_layer(self, num_new_layer):
+ #       for layer in range(num_new_layer):
+  #          self.layers = self.layers.append(Layer(sel))
+   #     self.num_layer += num_new_layer
+
+    #def remove_layer(self, num_remove_layer):
+     #   for layer in range(num_remove_layer):
+      #      self.layers = self.layers.append(Layer(self.inputs, self.weights))
+       # self.num_layer -= num_remove_layer
--- Classes/Layer.py
+++ Classes/Layer.py
@@ -1,37 +1,30 @@
 import numpy as np
-from Classes.Neuron import *
+# from Classes.Neuron import *
 class Layer():
-    def __init__(self, neuron_count):
-        self.neuron_count = neuron_count
-        self. neurons = []
-        for neuron in range(neuron_count):
-            self.neurons.append(Neuron(self.inputs))
-        self.input_vector = self.neurons[1].inputs  # all neurons in that layer take the same inputs
+    def __init__(self, begin_neuron_count, end_neuron_count, input=None):
+        self.begin_neuron_count = begin_neuron_count
+        self.end_neuron_count = end_neuron_count
+        self.input_vector = input
+        self.weight_matrix = np.transpose(np.random.rand(self.begin_neuron_count + 1, self.end_neuron_count))
 
-        self.MatrixUpdate()
-        self.output_vector = np.matmul(self.input_vector, self.layer_weight_matrix)
+        if self.input_vector != None:
+           self.output_vector = np.matmul(self.input_vector, self.weight_matrix)
 
-    def MatrixUpdate(self): # put the weights
-        num_neuron = len(self.neurons)  # neurons in this layer
-        num_next_neuron = len(self.neurons.in_vector)  # neurons in the next layer
-        l_mat = []
-        for n in range(num_neuron):
-            l_mat = np.append(l_mat, self.neurons.inputs)
-        l_mat = np.reshape(l_mat, (num_neuron, num_next_neuron))
-        self.layer_weight_matrix = l_mat
+    def take_input(self, input):
+        self.input_vector = input
 
-    def add_neuron(self, num_new_neuron):  # num_new_neuron contains the number of new neurons to be added to layer
-        for neuron in range(num_new_neuron):
-            self.neurons = self.neurons.append(Neuron(self.inputs))
-        self.neuron_count += num_new_neuron
-
-    def remove_neuron(self, num_remove_neuron):
-        for neuron in range(
-                num_remove_neuron):  # num_remove_neuron contains the number of new neurons to be added to layer
-            self.neurons = np.delete(self.neurons, -1)  # deletes the last element in list, kind  of arbitrary
-        self.node_count -= num_remove_neuron
+    def weight_matrix_update(self, loss_derivative, l_rate):  # put the weights
+        self.weight_matrix -= l_rate*loss_derivative
 
     def layer_output(self):
-        self.output_vector = np.matmul(self.input_vector, self.layer_weight_matrix)
+        self.output_vector = np.matmul(self.input_vector, self.weight_matrix)
 
 
+ #   def add_neuron(self, num_new_neuron, inputs):  # num_new_neuron contains the number of new neurons to be added to layer
+  #      self.begin_neuron_count =
+
+   # def remove_neuron(self, num_remove_neuron):
+    #    for neuron in range(
+     #           num_remove_neuron):  # num_remove_neuron contains the number of new neurons to be added to layer
+      #      self.neurons = np.delete(self.neurons, -1)  # deletes the last element in list, kind  of arbitrary
+       # self.neuron_count -= num_remove_neuron
