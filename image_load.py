0c7220b0c3861a1a887f7dd99ebe2168d02aff55
Cicadabuzz <efedolon@hotmail.com>
2021-04-02 01:15:23 +0300

    cv2 yüzünden ağlicam

--- image_load.py
+++ image_load.py
@@ -1,10 +1,27 @@
 import os
+import numpy as np
+import cv2
+
 ProjectFolder = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 directory_name = os.path.dirname
 imgFolder = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images"))
 imgTrain = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images", "Train"))
 
-imgTrainFlamingo = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images", "Train", "FLAMINGO"))
-#imgTrainFlamingo = imgTrainFlamingo.replace("\\", "/")
-imgTrainBarnOwl = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "Images", "Train", "BARN OWL"))
-#imgTrainBarnOwl = imgTrainFlamingo.replace("\\", "/")
+
+birdList = os.listdir(os.path.join(os.path.abspath(os.getcwd()), "Images", "Train"))
+birdDict = {"FLAMINGO":os.listdir(os.path.join(os.path.abspath(os.getcwd()), "Images", "Train", "FLAMINGO")),
+            "BARN OWL":os.listdir(os.path.join(os.path.abspath(os.getcwd()), "Images", "Train", "BARN OWL"))}
+imageNum = 0
+for bird in birdList:
+    imageNum += len(birdDict[bird])
+
+np.random.Generator.shuffle(birdList)
+for i in range(imageNum):
+    if i > imageNum/5:
+        randBird = np.random.Generator.choice(birdList)
+        randImg = np.random.Generator.choice(birdDict[randBird])
+        trainSet
+    else:
+        testSet
+
+
