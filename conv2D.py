67f35da6ca887b28a68824075551b0b832f68bb7
Cicadabuzz <efedolon@hotmail.com>
2021-04-01 19:50:02 +0300

    conv tweak2

--- conv2D.py
+++ conv2D.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 
 class conv2D:
 
-    def __init__(self, input, kernelNum, kernelSize, stride = 1)
+    def __init__(self, input, kernelNum, kernelSize, stride = 1):
         self.input = input
         self.kernelNum = kernelNum
         self.kernelSize = kernelSize
@@ -16,12 +16,12 @@ class conv2D:
 
     def convolve(self):
         """Convolve RGB image with multiple kernels and sum for feature map"""
-        convOut = np.empty((self.outputSize,self.outputSize,kernelNum)) # 3d array to store conv2d outputs
-        for kernel_i in range(kernelNum): # goes through each 2d kernel
+        convOut = np.empty((self.outputSize,self.outputSize,self.kernelNum)) # 3d array to store conv2d outputs
+        for kernel_i in range(self.kernelNum): # goes through each 2d kernel
             temp = np.zeros((self.convSize,self.convSize)) # to sum channels into one
-            for C in range(input.shape()[2]):
-                for i in range(0,self.inputSize - np.ceil((kernelSize-1)/2),stride): #start top-left move until kernel hits edge
-                    for j in range(0,self.inputSize - np.ceil((kernelSize-1)/2),stride):
-                        temp += input[i:i+kernelSize,j:j+kernelSize,C]*self.kernelMatrix[:,:,kernel_i]
+            for C in range(self.input.shape()[2]):
+                for i in range(0,self.inputSize - np.ceil((self.kernelSize-1)/2),self.stride): #start top-left move until kernel hits edge
+                    for j in range(0,self.inputSize - np.ceil((self.kernelSize-1)/2),self.stride):
+                        temp += input[i:i+self.kernelSize,j:j+self.kernelSize,C]*self.kernelMatrix[:,:,kernel_i]
             convOut[:,:,kernel_i] = temp
         self.convOut = np.maximum(convOut,np.zeros_like(convOut)) # apply ReLU before output
--- pooling.py
+++ pooling.py
@@ -3,14 +3,17 @@ import numpy as np
 class poolingLayer:
 
     def __init__(self, matrix, outputSize, ptype = 'max'):
+        self.matrix = matrix
+        self.outputSize = outputSize
+        self.ptype = ptype
         self.cellSize = matrix.shape()[0]//outputSize
 
-    def pool(self, matrix, outputSize, ptype):
-        temp = np.empty(outputSize,outputSize,matrix.shape()[2])
-        for i in range(0,matrix.shape()[0],self.cellSize):
-            for j in range(0,matrix.shape()[0],self.cellSize):
-                if ptype == 'max':
+    def pool(self):
+        temp = np.empty(self.outputSize,self.outputSize,self.matrix.shape()[2])
+        for i in range(0,self.matrix.shape()[0],self.cellSize):
+            for j in range(0,self.matrix.shape()[0],self.cellSize):
+                if self.ptype == 'max':
                     temp[i/self.cellSize,j/self.cellSize] = np.max(matrix[i:i+self.cellSize, j:j+self.cellSize])
-                if ptype == 'mean':
+                if self.ptype == 'mean':
                     temp[i/self.cellSize,j/self.cellSize] = np.mean(matrix[i:i+self.cellSize, j:j+self.cellSize])
-        return temp
+        self.pooled = temp
