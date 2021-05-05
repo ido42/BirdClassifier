4fe5040e054ff8848d3df040690f495a48471028
Cicadabuzz <efedolon@hotmail.com>
2021-05-05 22:04:23 +0300

    pooling done??!?!?!?

--- pooling.py
+++ pooling.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 class poolingLayer:
-    def __init__(self, outputSize, ptype = 'max'):
+    def __init__(self, outputSize):
         self.err = 0
         self.outputSize = outputSize
-        self.ptype = ptype
         self.cellSize = None
         self.positions = []
 
@@ -16,17 +15,14 @@ class poolingLayer:
         for kernel_i in np.arange(0, self.kernelNum,dtype="int"):
             for i in np.arange(0, self.outputSize,dtype="int"):
                 for j in np.arange(0,self.outputSize,dtype="int"):
-                    if self.ptype == 'max':
-                        cell = matrix[i*self.cellSize:(i+1)*self.cellSize, j*self.cellSize:(j+1)*self.cellSize, kernel_i]
-                        cellMax = np.max(cell)
-                        temp[i, j, kernel_i] = cellMax
-                        ind = np.where(cell == cellMax)
-                        if cellMax == 0:
-                            self.positions[i*self.cellSize, j*self.cellSize, kernel_i] = 1
-                        else:
-                            self.positions[i*self.cellSize+ind[0][0], j*self.cellSize+ind[1][0], kernel_i] = 1
-                    if self.ptype == 'mean':
-                        temp[i, j] = np.mean(matrix[i*self.cellSize:(i+1)*self.cellSize, j*self.cellSize:(j+1)*self.cellSize])
+                    cell = matrix[i*self.cellSize:(i+1)*self.cellSize, j*self.cellSize:(j+1)*self.cellSize, kernel_i]
+                    cellMax = np.max(cell)
+                    temp[i, j, kernel_i] = cellMax
+                    ind = np.where(cell == cellMax)
+                    if cellMax == 0:
+                        self.positions[i*self.cellSize, j*self.cellSize, kernel_i] = 1
+                    else:
+                        self.positions[i*self.cellSize+ind[0][0], j*self.cellSize+ind[1][0], kernel_i] = 1
         self.pooled = temp
         return self.pooled
 
